9b8b946bff8137c0386014a7886d303017b7a28f
Aroon <baralbhuwan6@gmail.com>
2026-01-02 21:53:26 +0545

    day 2 control flow python

---
+++ conditional2.py
@@ -0,0 +1,31 @@
+#Nested if..else Conditional Statement
+age = 70
+is_member = True
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+#Ternary Conditional Statement
+#A ternary conditional statement is a compact way to write an if-else condition in a single line. 
+# It’s sometimes called a "conditional expression."
+
+# Assign a value based on a condition
+age = 20
+s = "Adult" if age >= 18 else "Minor"
+print(s)
+
+#Match case Statment
+number = 2
+
+match number:
+    case 1:
+        print("One")
+    case 2 | 3:
+        print("Two or Three")
+    case _:
+        print("Other number")
