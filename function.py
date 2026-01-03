8c55b5fd74a0608764635b9d270ce622c3fda514
Aroon <baralbhuwan6@gmail.com>
2026-01-03 21:53:41 +0545

    day 3 control flow python

--- function.py
+++ function.py
@@ -1,3 +1,13 @@
-def fun():
-    pass
-fun()
+class EmptyClass:
+    pass  # No methods or attributes yet
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        pass  # Placeholder for greet method
+
+# Creating an instance of the class
+p = Person("Emily", 30)
