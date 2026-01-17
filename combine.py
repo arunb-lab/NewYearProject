84d8de5daf7681e06ec5f806ae4f5a020fcbe91e
Aroon <baralbhuwan6@gmail.com>
2026-01-17 21:07:23 +0545

    basics

---
+++ combine.py
@@ -0,0 +1,10 @@
+f_name = "ada"
+l_name = "lovelace"
+full_name = f_name + " " + l_name
+print(full_name)
+print(full_name.title())
+
+print("Hello, " + full_name.title() + "!")
+
+message = "Oi Hey there, " + full_name.title() + "!"
+print(message)
