859440916dc6976ea80125e28abbbec6c6c895e3
Aroon <baralbhuwan6@gmail.com>
2026-01-17 22:07:52 +0545

    basics

---
+++ format.py
@@ -0,0 +1,9 @@
+#whitespace
+favorite_language = ' python  '
+print(favorite_language)
+print("\tPython")
+
+#newline
+print("My favourite languguage is:\n\tPython\n\tJavaScript\n\tC\n\tC++")
+
+print(favorite_language.rstrip())
