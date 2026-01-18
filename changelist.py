39f4fff26470d46b9ff2eb799fb4b72cf7efa429
Aroon <baralbhuwan6@gmail.com>
2026-01-18 21:50:44 +0545

    lists

---
+++ changelist.py
@@ -0,0 +1,16 @@
+motorcycle = ['honda', 'yamaha', 'suzuki']
+print(motorcycle)
+
+motorcycle[0] = 'KTM Duke'
+print(motorcycle)
+
+motorcycle.append('Tryumph')
+print(motorcycle)
+
+bike = []
+bike.append('Ducati')
+bike.append('Harley Davidson')
+bike.append('BMW')
+bike.append('Royal Enfield')
+
+print(bike)
