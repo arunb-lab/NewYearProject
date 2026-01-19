1c862fb888960c37cf37aa0aa3147789875a27bb
Aroon <baralbhuwan6@gmail.com>
2026-01-19 20:46:57 +0545

    loop

--- looplist.py
+++ looplist.py
@@ -2,3 +2,6 @@ magicians = ['alice', 'bob', 'carol','Arun']
 
 for magician in magicians:
     print(magician)
+
+    print(magician.title() + ", That was a great trick wow!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
