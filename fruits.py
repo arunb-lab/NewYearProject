fcc3555995983d457c1facedbb03d1bad99c4ed7
Aroon <baralbhuwan6@gmail.com>
2026-01-27 21:59:22 +0545

    elif Python

---
+++ fruits.py
@@ -0,0 +1,17 @@
+favorite_fruits = ['mango', 'strawberry', 'pineapple']
+
+# Check for different fruits using independent if statements
+if 'mango' in favorite_fruits:
+    print("You really like mangoes!")
+
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+
+if 'pineapple' in favorite_fruits:
+    print("You really like pineapples!")
