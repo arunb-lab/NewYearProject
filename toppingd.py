803abe2d9ac3d26ed9c7d2639113fc3667293bc6
Aroon <baralbhuwan6@gmail.com>
2026-01-29 21:33:41 +0545

    empty list with if else and for loop

--- toppingd.py
+++ toppingd.py
@@ -2,7 +2,6 @@ requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 for requested_topping in requested_toppings:
     if requested_topping == 'green peppers':
         print("Sorry, we are out of green peppers right now.")
-        else:
-
- print("Adding " + requested_topping + ".")
+    else:
+        print("Adding " + requested_topping + ".")
 print("\nFinished making your pizza!")
